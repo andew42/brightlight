572a0c373a9589d5a82f1fc6f75a3e051bc84f05
andrew <andrew@anotherurl.com>
2015-07-29 08:51:58 +0100

    Serve static content via Go library (http.Dir) rather than local file cache

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/andew42/brightlight/animations"
 	"github.com/andew42/brightlight/controller"
 	"github.com/andew42/brightlight/stats"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,57 +19,6 @@ import (
 // The controller's frame buffer
 var fb = controller.NewFrameBuffer()
 var Statistics stats.Stats = stats.NewStats()
-
-// Read each file's content, in the content directory, into memory against its relative path
-func cacheContentDirectory(path string, basePathLen int, cache map[string][]byte) error {
-	info, err := ioutil.ReadDir(path)
-	if err != nil {
-		return err
-	}
-	for _, i := range info {
-		itemPath := path + "/" + i.Name()
-		if i.IsDir() {
-			err = cacheContentDirectory(itemPath, basePathLen, cache)
-			if err != nil {
-				return err
-			}
-		} else {
-			cache[itemPath[basePathLen:]], err = ioutil.ReadFile(itemPath)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// Handle HTTP requests for static content
-func staticContentHandler(w http.ResponseWriter, r *http.Request) {
-	// Crudely determine content type from path extension
-	extIndex := strings.LastIndex(r.URL.Path, ".")
-	if extIndex == -1 {
-		http.Error(w, "Content file has no extension", 406)
-		return
-	}
-	// Look up path in cache
-	content, ok := contentCache[r.URL.Path]
-	if !ok {
-		http.Error(w, "File not found", 404)
-		return
-	}
-	// Return content with correct type in header
-	contentType := ""
-	switch r.URL.Path[extIndex+1:] {
-	case "js":
-		contentType = "application/javascript"
-		// TODO: Add HTML Type?
-	default:
-		contentType = "text/" + r.URL.Path[extIndex+1:]
-	}
-	w.Header().Set("Content-Type", contentType)
-	fmt.Printf("%v CONTENT TYPE %v\n", r.URL.Path, contentType)
-	w.Write(content)
-}
 
 // Render the frame buffer as JSON
 func sendFrameBufferToWebSocket(ws *websocket.Conn) error {
@@ -210,44 +158,34 @@ func statsSocketHandler(ws *websocket.Conn) {
 	}
 }
 
-// HTTP content cache
-var contentCache = make(map[string][]byte)
-
 func main() {
 	// What are we running on?
 	fmt.Printf("environment: %v %v %v\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
-
-	// Figure out where the content directory is GOPATH may contain : separated paths
-	goPath := strings.Split(os.Getenv("GOPATH"), ":")
-	contentPath := path.Join(goPath[0], "src/github.com/andew42/brightlight/ui")
-
-	// Create an in memory cache of the content directory
-	err := cacheContentDirectory(contentPath, len(contentPath), contentCache)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// TODO: Print out cache summary
-	for k, v := range contentCache {
-		fmt.Println(k, len(v))
-	}
 
 	// Start serial and animation drivers
 	controller.StartDriver(fb, &Statistics)
 	animations.StartDriver(fb, &Statistics)
 
-	// Start web handlers
-	http.HandleFunc("/", staticContentHandler)
+	// Figure out where the content directory is GOPATH may contain : separated paths
+	goPath := strings.Split(os.Getenv("GOPATH"), ":")
+	contentPath := path.Join(goPath[0], "src/github.com/andew42/brightlight/ui")
+
+	// Set up web routes (first static content)
+	fs := http.FileServer(http.Dir(contentPath))
+	http.Handle("/", fs)
+	// Requests to turn on all lights
 	http.HandleFunc("/AllLights/", allLightsHandler)
+	// Requests to run an animation
 	http.HandleFunc("/Animation/", animationHandler)
+	// Requests to show a configuration on the room lights
 	http.HandleFunc("/Config/", configHandler)
+	// Push frame buffer changes over a web socket
 	http.Handle("/FrameBuffer", websocket.Handler(frameBufferSocketHandler))
+	// Push stats info over a web socket
 	http.Handle("/Stats", websocket.Handler(statsSocketHandler))
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
+	// Start web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Println("ListenAndServe: " + err.Error())
 	}
-
 	fmt.Print("main exiting")
 }
