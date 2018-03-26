4603e9d896e976803f5ee776d7318c58d3409cd1
andew42 <andrew@anotherurl.com>
2018-03-26 07:54:29 +0100

    Fix check box selection for segments in ui2

--- ui2/src/button-edit/ButtonEdit.js
+++ ui2/src/button-edit/ButtonEdit.js
@@ -12,27 +12,27 @@ export default class ButtonEdit extends React.Component {
     constructor(props) {
         super(props);
 
-        // TODO
-        this.segmentList = ['Static', 'Twinkle', 'Rainbow'];
-
         // We expect to be passed all buttons and button to edit in location state
         this.buttons = props.history.location.state.buttons;
         this.button = props.history.location.state.button;
 
+        // Copy the initial state of the button we are editing
+        let buttonSegmentsCopy = JSON.parse(JSON.stringify(this.button.segments));
         this.state = {
-            // Initial state of button we are editing
             name: this.button.name,
-            segments: JSON.parse(JSON.stringify(this.button.segments)),
-// TODO            selectedSegments: this.state.segments.map(s => s.segment)
+            segments: buttonSegmentsCopy,
+            selectedSegments: buttonSegmentsCopy.map(s => s.segment)
         };
+
+        // Callback bindings for this
         this.onOK = this.onOK.bind(this);
-        this.onAddSegment = this.onAddSegment.bind(this);
+        this.toggleSelectedSegment = this.toggleSelectedSegment.bind(this);
     }
 
     onOK() {
         // Update button object and save to server
         this.button.name = this.state.name;
-        // TODO HANDLE ERRORS BETTER
+        // TODO: Better error handling
         saveButtons(this.buttons, () => this.props.history.goBack(), e => console.error(e));
     }
 
@@ -41,21 +41,19 @@ export default class ButtonEdit extends React.Component {
         this.setState({segments: this.state.segments.filter(seg => seg !== segment)})
     }
 
-    onAddSegment() {
-        console.info('adding segment');
-        // TODO
-    }
-
     toggleSelectedSegment(segName) {
-        // TODO
-        // if (state.selectedSegments.includes(segName))
-        //     this.setState({selectedSegments: state.selectedSegments.filter(n => n != segName)});
-        // else
-        //     this.setState({selectedSegments: state.s})
+        console.info('Toggle:' + segName);
+        if (this.state.selectedSegments.includes(segName))
+            this.setState({selectedSegments: this.state.selectedSegments.filter(n => n !== segName)});
+        else {
+            let selectedSegmentCopy = JSON.parse(JSON.stringify(this.state.selectedSegments));
+            selectedSegmentCopy.push(segName);
+            this.setState({selectedSegments: selectedSegmentCopy});
+        }
     }
 
     render() {
-        let selectedSegments = this.state.segments.map(s => s.segment);
+        //let selectedSegments = this.state.segments.map(s => s.segment);
         let key = 1;
         return <div className="button-edit-editor-list">
             <Fragment>
@@ -65,9 +63,9 @@ export default class ButtonEdit extends React.Component {
                     <LedSegment key={key++} segment={segment} onRemove={seg => this.onRemoveSegment(seg)}/>))}
 
                 <div className='ok-cancel-container'>
-                    <LedSegmentList selectedItems={selectedSegments}
-                                    toggleSelectedSegment={seg => console.info(seg)}
-                                    trigger={<Button icon='plus' circular floated='left'></Button>}/>
+                    <LedSegmentList selectedItems={this.state.selectedSegments}
+                                    toggleSelectedSegment={seg => this.toggleSelectedSegment(seg)}
+                                    trigger={<Button icon='plus' circular floated='left'/>}/>
                     <Button primary onClick={this.onOK}>OK</Button>
                     <Button secondary onClick={() => this.props.history.goBack()}>Cancel</Button>
                 </div>
