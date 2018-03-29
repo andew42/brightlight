a2e738dcee1ddeacef5ce16d0bdfb3aa865315a6
andew42 <andrew@anotherurl.com>
2018-03-29 07:49:49 +0100

    Fix check box selection for segments in ui2

--- ui2/src/button-edit/LedSegment.js
+++ ui2/src/button-edit/LedSegment.js
@@ -1,15 +1,20 @@
 import * as React from "react";
-import {Label, Segment} from "semantic-ui-react";
+import {Image, Label, Segment} from "semantic-ui-react";
+import './LedSegment.css';
 
 export default class LedSegment extends React.Component {
 
     render() {
         let segment = this.props.segment;
-        return <Segment attached color='blue'>
+        return <Segment attached color='blue' className='led-segment-container'>
             <Label content={segment.segment}
                    onRemove={() => this.props.onRemove(segment)}
                    color='blue' attached='top left'/>
-            <div>{segment.animation}<br/>{segment.params}</div>
+            <Image className='led-segment-image'
+                   size='tiny'
+                   inline
+                   src={"/segment-icons/" + encodeURIComponent(segment.segment) + ".svg"}/>
+            <span>{segment.animation}<br/>{segment.params}</span>
         </Segment>;
     }
 }
