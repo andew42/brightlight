aaaa44dbe76bd0f14300d9ddaea3ee10d1ac0c29
andew42 <andrew@anotherurl.com>
2018-08-28 07:56:08 +0100

    Allow multiple copies of the same segment to be defined in a button This allows animations to be composed e.g. cylon over static

--- ui2/src/button-edit/ButtonEditor.js
+++ ui2/src/button-edit/ButtonEditor.js
@@ -9,6 +9,11 @@ import {Button} from "semantic-ui-react";
 // Shows a list of editors that change to suit the animation
 export default class ButtonEditor extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {selectedSegments: []};
+    };
+
     componentDidMount() {
         // Make a copy of the initial button state in case we cancel
         let allButtons = this.props.allButtons;
@@ -22,27 +27,56 @@ export default class ButtonEditor extends React.Component {
         let allButtons = this.props.allButtons;
         let buttonKey = this.props.history.location.state.buttonKey;
         let button = allButtons.find(x => x.key === buttonKey);
+
         // Make a note of initial button in case we cancel
         if (this.preSegChooserButton === undefined)
             this.preSegChooserButton = {...button};
-        if (button.segments.find(s => s.name === seg.name) === undefined) {
-            // Add new segment to the button
+
+        // Work out top z order for this segment name as there
+        // may be zero or more segments with this name
+        let topz = button.segments.filter(s => s.name === seg.name).map(s => s.z).reduce((_, current, index, array) => Math.max(current, array[index]), undefined);
+
+        // Is this a selection or a removal (check collection for this add popup)
+        if (this.state.selectedSegments.find(s => s === seg.name) === undefined) {
+
+            // Add NEW segment to check collection and button
+            let newSelectedSegments = this.state.selectedSegments.map(x => x);
+            newSelectedSegments.push(seg.name);
+            this.setState({selectedSegments: newSelectedSegments});
+
+            // Make a copy of the new segments
             let newSegments = button.segments.map(x => x);
-            newSegments.push({
+
+            // New segment to add with a static animation
+            let newSegment = {
                 ...seg, animation: "Static", params: [
                     {"key": 20, "type": "colour", "label": "Colour", "value": {"r": 31, "g": 31, "b": 31}}]
-            });
+            };
+
+            // Adjust new segment Z order if necessary (already segment(s) with this name)
+            if (topz !== undefined)
+                newSegment.z = topz + 0.1;
+
+            // Add new segment to the new segments copy
+            newSegments.push(newSegment);
+
+            // Sort the segments into z order
             newSegments.sort((a, b) => a.z - b.z);
+
+            // Update the button so we see changes immediately
             this.props.onButtonChanged({
                 ...button,
                 segments: newSegments
             });
         }
         else {
-            // Remove segment from button
+            // REMOVE segment from check collection...
+            this.setState({selectedSegments: this.state.selectedSegments.filter(s => s !== seg.name)});
+
+            // ...and button
             this.props.onButtonChanged({
                 ...button,
-                segments: button.segments.filter(s => s.name !== seg.name)
+                segments: button.segments.filter(s => s.name !== seg.name || s.z !== topz)
             });
         }
     }
@@ -73,22 +107,26 @@ export default class ButtonEditor extends React.Component {
                                       allAnimationNames={animationNames}
                                       onRemove={seg => this.props.onButtonChanged({
                                           ...button,
-                                          segments: button.segments.filter(s => s.name !== seg.name)
+                                          segments: button.segments.filter(s => s.name !== seg.name || s.z !== seg.z)
                                       })}
                                       onSegmentChanged={seg => this.props.onButtonChanged({
                                           ...button,
-                                          segments: button.segments.map(s => s.name === seg.name ? seg : s)
+                                          segments: button.segments.map(s => s.name === seg.name && s.z === seg.z ? seg : s)
                                       })}/>))}
 
                 <div className='button-editor-ok-cancel-container'>
                     <LedSegmentChooser allSegments={allSegments}
-                                       checkedSegmentNames={button.segments.map(s => s.name)}
-                                       onOk={() => this.preSegChooserButton = undefined}
+                                       checkedSegmentNames={this.state.selectedSegments}
+                                       onOk={() => {
+                                           this.preSegChooserButton = undefined;
+                                           this.setState({selectedSegments: []});
+                                       }}
                                        onCancel={() => {
                                            if (this.preSegChooserButton !== undefined) {
                                                this.props.onButtonChanged(this.preSegChooserButton);
                                                this.preSegChooserButton = undefined;
                                            }
+                                           this.setState({selectedSegments: []});
                                        }}
                                        toggleCheckedSegment={seg => this.toggleSelectedSegment(seg)}
                                        trigger={<Button icon='plus'
